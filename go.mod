2acd6f600cd937ed7acf7643783231ff2eba9991
unknown <topget@sina.com>
2021-07-10 17:00:49 +0800

    2021071001

--- go.mod
+++ go.mod
@@ -37,3 +37,6 @@ replace github.com/topxeq/xmlx v0.2.0 => ../xmlx
 replace github.com/topxeq/sqltk v0.0.0 => ../sqltk
 
 replace github.com/topxeq/qlang v0.0.0 => ../qlang
+
+replace github.com/topxeq/text v0.0.0 => ../text
+
--- goxc.go
+++ goxc.go
@@ -166,7 +166,7 @@ import (
 
 // Non GUI related
 
-var versionG = "1.75a"
+var versionG = "1.76a"
 
 // add tk.ToJSONX
 
@@ -1271,6 +1271,7 @@ func importQLNonGUIPackages() {
 		"jsonEncode":   tk.ObjectToJSON,
 		"jsonDecode":   tk.JSONToObject,
 		"toJSON":       tk.ToJSONX,
+		"fromJSON":     tk.FromJSONWithDefault,
 		"simpleEncode": tk.EncodeStringCustomEx,
 		"simpleDecode": tk.DecodeStringCustom,
 
